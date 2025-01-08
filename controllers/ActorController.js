ba0b5c7ab634ca7737d4051f0deaedd5d93b7b5d
patricklohn <patricklohn3@gmail.com>
2025-01-08 00:10:08 -0300

    criando actor

--- controllers/ActorController.js
+++ controllers/ActorController.js
@@ -9,5 +9,38 @@ module.exports = class ActorController{
     static showClient(req,res){
         res.render('actors/showClient')
     }
+
+    static async addActor(req,res){
+
+        let client = req.body.client;
+        let supplier = req.body.supplier;
+
+        if(client === 'on'){
+            client = true
+        }else{
+            client = false
+        }
+
+        if(supplier === 'on'){
+            supplier = true
+        }else{
+            supplier = false
+        }
+
+
+        const actor = {
+            name: req.body.name,
+            client,
+            supplier,
+            contact: req.body.contact,
+            contact_number: req.body.contact_number, 
+            email: req.body.email,
+        }
+
+        await Actor.create(actor)
+
+        res.redirect('/')
+
+    }
     
 } 
--- routes/actorsRoutes.js
+++ routes/actorsRoutes.js
@@ -5,5 +5,6 @@ const ActorController = require('../controllers/ActorController')
 
 router.get('/createActors', ActorController.createActor)
 router.get('/showClient', ActorController.showClient)
+router.post('/add-actor', ActorController.addActor)
 
 module.exports = router;
